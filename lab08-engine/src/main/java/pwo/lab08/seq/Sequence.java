56727fd515ad13407ccc3025b601465748861469
student <student@localhost>
2022-11-09 18:55:25 +0100

    lab08

---
+++ lab08-engine/src/main/java/pwo/lab08/seq/Sequence.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pwo.lab08.seq;
+
+/**
+ *
+ * @author student
+ */
+public class Sequence {
+    public static final String VERSION = "Number sequences sequence version 1.0.0";
+
+    public static String getVersion() {
+        return VERSION;
+    }
+}
---
+++ lab08-engine/src/main/java/pwo/lab08/utils/utils.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pwo.lab08.utils;
+
+/**
+ *
+ * @author student
+ */
+public class utils {
+    public static final String VERSION = "Number sequences utils version 1.0.0";
+
+    public static String getVersion() {
+        return VERSION;
+    }
+}
